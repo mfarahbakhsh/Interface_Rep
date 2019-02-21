8745c8b9d92276e8cc4706db0fe7fc9cc5e7cc88
farahbakhsh <farahbakhsh2014@gmail.com>
2019-02-21 11:42:49 +0330

    multiple inheritance

---
+++ src/interface_main/A4.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package interface_main;
+
+/**
+ *
+ * @author farsam
+ */
+public class A4 implements Printable,Showable{
+   public void print(){
+        System.out.println("print A4");
+    }
+   public void show(){
+         System.out.println("show A4");
+    }
+}
