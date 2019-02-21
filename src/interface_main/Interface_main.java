27e331fcac05c6797aad2d66e7ff32b9a71cdddf
farahbakhsh <farahbakhsh2014@gmail.com>
2019-02-21 11:38:05 +0330

    multiple inheritance in java

--- src/interface_main/Interface_main.java
+++ src/interface_main/Interface_main.java
@@ -17,9 +17,33 @@ public class Interface_main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       Dog d=new Dog();
+       Animal d=new Dog();
        d.eat();
        d.move();
+       Animal a;
+    //  a.eat();
+        a = new Animal() {
+            
+            @Override
+            public void eat() {
+               
+                System.out.println("eat from override");
+            }
+            
+            @Override
+            public void move() {
+                
+                System.out.println("move from override");
+                
+            }
+        };
+        a.eat();
+        a.move();
+        
+        Printable p=new A4();
+        Showable  s=new A4();
+        p.print();
+        s.show();
       
     }
     
